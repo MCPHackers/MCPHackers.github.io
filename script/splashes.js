d634efc1f067668093cd91fe1dcda9b6bafa6159
Lassebq <lassebq.mine@gmail.com>
2022-12-29 13:15:03 +0200

    Random splash easter egg. VersionsV2 json

--- script/splashes.js
+++ script/splashes.js
@@ -1,6 +1,5 @@
 //TODO make it so splash size is calculated from string length
-const randomSplash = false;
-const splashSizes = [2, 2, 2, 1.7, 2, 1.8, 0.8, 1.2, 1, 1.4, 1.9];
+const splashSizes = [2, 2, 2, 1.7, 2, 1.8, 0.8, 1.2, 1, 1.4, 1.9, 2, 2, 2, 1.7];
 const splashes = [
 "HACKERS",
 "Uses HTML!",
@@ -12,19 +11,24 @@ const splashes = [
 "RetroMCP-Python is cringe!",
 "Welcome to the Los Pollos hermanos family",
 "Try to center a &lt;div&gt;",
-"<span class=\"darkblue\">C</span><span class=\"darkgreen\">o</span><span class=\"darkaqua\">l</span><span class=\"darkred\">o</span><span class=\"darkpurple\">r</span><span class=\"gold\">m</span><span class=\"gray\">a</span><span class=\"darkgray\">t</span><span class=\"blue\">i</span><span class=\"green\">c</span>"
+"<span class=\"darkblue\">C</span><span class=\"darkgreen\">o</span><span class=\"darkaqua\">l</span><span class=\"darkred\">o</span><span class=\"darkpurple\">r</span><span class=\"gold\">m</span><span class=\"gray\">a</span><span class=\"darkgray\">t</span><span class=\"blue\">i</span><span class=\"green\">c</span>",
+"Infdev!",
+"Alpha!",
+"Beta!",
+"Better than MCP!"
 ];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function splash() {
+function splash(randomSplash) {
 	const splash = document.getElementById("randomsplash");
 	var index = 0;
 	if(randomSplash) {
 		index = getRandomInt(splashes.length);
 	}
+	var size = splashSizes[index];
 	splash.innerHTML = splashes[index];
-	splash.style.cssText = "--scale:" + splashSizes[index];
+	splash.style.cssText = "--scale:" + size;
 }
